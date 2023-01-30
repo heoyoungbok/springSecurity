9ff5ee86c64ccf8a7766452a699145f263eb517e
user <tinyhyb@naver.com>
2023-01-30 17:37:57 +0900

    12_홈페이지 웹 완성 13_웹 시작 전역클릭 이벤트
    -> toty소식 게시판 커뮤니티 게시판 -> 무한 스크롤 대기중
    
    관리자 페이지 , 메뉴 권한 마무리
    
    save 매서드 동작x 해결중

--- src/main/java/com/its/springsecurity/controller/UserController.java
+++ src/main/java/com/its/springsecurity/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.its.springsecurity.controller;
 
+import com.its.springsecurity.domain.User;
+import com.its.springsecurity.domain.user.UserDTO;
 import com.its.springsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,7 +19,11 @@ public class UserController {
         return "save";
     }
 
+    @PostMapping("/save")
+        public String createUser(@ModelAttribute UserDTO userDTO){
+            userService.save(User.toSaveEntity(userDTO));
+            return "login";
 
+        }
 
-
-}
+    }
